51427360d137ff2fc91aefbb38cb020dafaf0b73
GReeN-LT <mindaugas.laboga23@gmail.com>
2018-06-10 17:16:03 +0300

    Created Windows Form program.

---
+++ Snake/FormMain.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Snake
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMenu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMenu
+            // 
+            this.lblMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblMenu.Location = new System.Drawing.Point(0, 0);
+            this.lblMenu.Name = "lblMenu";
+            this.lblMenu.Size = new System.Drawing.Size(300, 200);
+            this.lblMenu.TabIndex = 0;
+            this.lblMenu.Text = "Snake\r\n\r\nUse the arrow keys to change directions\r\nUse the space bar to pause\r\n\r\nP" +
+    "ress enter to begin";
+            this.lblMenu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 200);
+            this.Controls.Add(this.lblMenu);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(316, 239);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(316, 239);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SnakeVisma -Score: 1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormMain_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormMain_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMenu;
+    }
+}
+
